b2e96ee51d809d58276a3f4ce8c04efd53c9f8ef
Daniel <danie.villano@gmail.com>
2020-06-12 20:23:12 -0300

    nada

--- calculator.js
+++ calculator.js
@@ -31,6 +31,6 @@ app.post("/bmicalculator", function(req,res){
     res.send("Your BMI is: " + result);
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(5000, function () {
+    console.log('Example app listening on port 5000!');
   });
